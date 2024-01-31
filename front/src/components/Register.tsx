d6db600d4c313db5ef108f9208651e465fae5b57
[Pedro Sota] <psptaier@gmail.com>
2024-01-31 15:38:25 -0300

    formulario de registro.

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -13,41 +13,48 @@ import {
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { isDirty, z } from "zod"
 
 
 const formSchema = z.object({
   nombre: z.string().min(2).max(50),
   apellido: z.string().min(2).max(50),
+  email: z.string().email(),
 })
 
 
 export default function Register() {
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       nombre: "",
       apellido: "",
+      email: "",
     },
   })
+ 
   function onSubmit(values: z.infer<typeof formSchema>) {    
     console.log(values)
   }
+
   return (
     
     <div className="flex flex-col justify-center items-center gap-y-6">
       <h1 className="font-semibold text-3xl">Crear cuenta</h1>
-      <Progress value={50} className="w-[346px]" />
+      <Progress value={50} className="w-[346px] mb-8" />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8 w-[460px]">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 items-center justify-between gap-x-4 gap-y-10 w-[460px]">
           <FormField
             control={form.control}
             name="nombre"            
             render={({ field }) => (
-              <FormItem className="max-w-[220px]">
-                <FormLabel>Username</FormLabel>
+              <FormItem className="">
+                <FormLabel className={form.control.getFieldState("nombre").isDirty ? " text-sm -top-6 -left-0" : "" } >
+                  Nombre
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input {...field}  />
                 </FormControl>                
                 <FormMessage />
               </FormItem>
@@ -57,16 +64,33 @@ export default function Register() {
             control={form.control}
             name="apellido"
             render={({ field }) => (
-              <FormItem className="max-w-[220px]">
-                <FormLabel>Apellido</FormLabel>
+              <FormItem className="">
+                <FormLabel className={form.control.getFieldState("apellido").isDirty ? " text-sm -top-6 -left-0" : "" }>
+                  Apellido
+                </FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input {...field} />
                 </FormControl>                
                 <FormMessage />
               </FormItem>
             )}            
           />
-          <Button type="submit" className="w-full" variant="outline" size="lg">Siguiente</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({field}) => (
+              <FormItem className="col-span-2">
+                <FormLabel className={form.control.getFieldState("email").isDirty ? " text-sm -top-6 -left-0" : "" }>
+                  Email
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="col-span-2 w-full" variant="outline" size="lg">Siguiente</Button>
         </form>
       </Form>
       <div className="flex flex-col justify-center items-center text-sm">
--- front/src/components/ui/label.tsx
+++ front/src/components/ui/label.tsx
@@ -7,8 +7,10 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "absolute top-5 left-4  group-focus-within:-top-6 group-focus-within:-left-0 group-focus-within:text-sm  font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 transform transition-all ease-in duration-200",  
 )
+
+
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
