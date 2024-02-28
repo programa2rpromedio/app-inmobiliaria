86a9fcced7a99984cc63755a6201f24195a58c37
Sebastian Gallego <sebastian.gallego@gmail.com>
2024-02-28 10:38:25 -0300

    test nuevo home

--- front/src/app/detalle/page.tsx
+++ front/src/app/detalle/page.tsx
@@ -1,29 +1,23 @@
-import Image from 'next/image'
-import HeroDetail from '@/components/HeroDetail'
-import Metrospng from '../../../public/metros.png'
-import Bañopng from '../../../public/baño.png'
-import Mascotapng from '../../../public/mascota.png'
-import Habitacionpng from '../../../public/habitacion.png'
-import Jardinpng from '../../../public/jardin.png'
-import Transportepng from '../../../public/transporte.png'
-import FigMap from '../../../public/Figmap.png'
-import VectorCasa from '../../../public/vector_casa.png'
-import FormContact from '@/components/FormContact'
-
-
-
-
+import Image from "next/image";
+import HeroDetail from "@/components/HeroDetail";
+import Metrospng from "../../../public/metros.png";
+import Bañopng from "../../../public/baño.png";
+import Mascotapng from "../../../public/mascota.png";
+import Habitacionpng from "../../../public/habitacion.png";
+import Jardinpng from "../../../public/jardin.png";
+import Transportepng from "../../../public/transporte.png";
+import FigMap from "../../../public/Figmap.png";
+import VectorCasa from "../../../public/vector_casa.png";
+import FormContact from "@/components/FormContact";
 
 export default function page() {
-
-
   return (
     <main className="w-8/12 mx-auto border border-black p-4">
       <HeroDetail />
 
-      <section className="mt-4 flex gap-16">
+      <section className="mt-4 flex flex-col lg:flex-row mx-auto items-center gap-16">
         <div>
-          <div className="flex gap-5">
+          <div className="flex gap-4">
             <div className="w-[85px] h-[91px] bg-secondary flex flex-col justify-center items-center rounded-lg">
               <Image
                 src={Metrospng}
@@ -88,9 +82,8 @@ export default function page() {
 
           <div className="w-[474px] mt-8">
             <p>
-              ¡Te presentamos el departamento ideal en Almagro!
-              A pocos metros de la linea B de Subte.
-              A 100 metros de Av. Corrientes
+              ¡Te presentamos el departamento ideal en Almagro! A pocos metros
+              de la linea B de Subte. A 100 metros de Av. Corrientes
             </p>
           </div>
 
@@ -105,29 +98,33 @@ export default function page() {
           </div>
         </div>
 
-
-        <div className=" w-[50%] bg-vectorcasa bg-contain">
-
+        <div className=" lg:w-[40%] bg-vectorcasa bg-cover bg-center">
           <FormContact />
         </div>
-
       </section>
 
-      <hr className='w-full mb-[25px] mt-[100px] border-[#CAC4D0]' />
+      <hr className="w-full mb-[25px] mt-[100px] border-[#CAC4D0]" />
 
-      <section className='my-[70px] flex flex-col gap-10'>
-        <div className='flex justify-center gap-10'>
-          <a href="" className='text-[24px]'>Descripción</a>
-          <a href="" className='text-[24px]'>Información Adicional</a>
+      <section className="my-[70px] flex flex-col gap-10">
+        <div className="flex justify-center gap-10">
+          <a href="" className="text-[24px]">
+            Descripción
+          </a>
+          <a href="" className="text-[24px]">
+            Información Adicional
+          </a>
         </div>
-        <div className='mb-[56px]'>
-          <p className='text-pretty'>
-            Patio cubiertoIngreso por patio techado de vidrio que separa la cocina de un ambiente.La propiedad se entrega con aire acondicionado y piso de parquet.Cocina con espacio de comedor, toda refaccionada. También tiene espacio para un lavarropas.Baño completo, con mampara.
+        <div className="mb-[56px]">
+          <p className="text-pretty">
+            Patio cubiertoIngreso por patio techado de vidrio que separa la
+            cocina de un ambiente.La propiedad se entrega con aire acondicionado
+            y piso de parquet.Cocina con espacio de comedor, toda refaccionada.
+            También tiene espacio para un lavarropas.Baño completo, con mampara.
           </p>
           <p> Hermosa y amplia terraza con lavadero.</p>
         </div>
 
-        <div className='flex justify-evenly w-full'>
+        <div className="flex justify-evenly w-full">
           <div>
             <ul>
               <li>Cantidad de ambientes: 2</li>
@@ -149,18 +146,14 @@ export default function page() {
             </ul>
           </div>
         </div>
-
       </section>
 
-      <hr className='w-full mb-[25px]  border-[#CAC4D0]' />
+      <hr className="w-full mb-[25px]  border-[#CAC4D0]" />
 
       <section>
         Otras propiedades que pueden ser para ti
         {/* CARDS PROPIEDADES */}
       </section>
-
-    </main >
-  )
+    </main>
+  );
 }
-
-
--- front/src/app/home2/page.tsx
+++ front/src/app/home2/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 import Logo from "@/images/logo.svg";
-
 import Search from "@/images/search.png";
 import Alquilar from "@/images/alquilar.svg";
 import Temporal from "@/images/temporal.svg";
@@ -9,43 +9,41 @@ import BuscaDescubre from "@/images/buscaDescubre.svg";
 import Conecta from "@/images/conectaCasa.svg";
 
 import Presentacion from "@/images/presentacion.svg";
-import { Button } from "@/components/ui/button";
 
-export default function home() {
+export default function page() {
   return (
-    <div className="bg-[#F5F5F7] ">
-      <header className="flex justify-between">
+    <main className=" mx-auto ">
+      <div className="flex justify-between items-center">
         <div className="flex items-center ">
           <Image
             src={Logo}
             alt="Logo de la App"
-            className=""
+            className="ml-2"
             width={40}
             height={40}
           />
-          <p className="text-sm font-semibold m-4"> Nombre App</p>
+          <p className="text-sm font-bold m-4"> Alquileres Ya!</p>
         </div>
         <Button variant="link">Iniciar sesión</Button>
-      </header>
+      </div>
 
       <section className="flex justify-center flex-col">
-        <p className="font-semibold text-4xl text-center whitespace-normal break-words sm:whitespace-nowrap sm:break-normal">
+        <p className="w-[250px] md:w-full mx-auto mb-5 font-bold text-[27px] text-center">
           ¡Tu hogar ideal en un solo click!
         </p>
 
-        <div className="flex  justify-center ">
+        <div className="flex  justify-center gap-5 ">
           <div className="flex  ">
             <div className="">
-              {" "}
               <Image
                 src={Alquilar}
                 alt="Logo de la App"
-                className="row-start-1 row-end-3 rounded-tl-[20px] rounded-bl-[20px] object-cover"
+                className=""
                 width={20}
                 height={20}
               />
             </div>
-            <p className="text-md mx-5">Alquilar</p>
+            <p className="text-md mx-2">Alquilar</p>
           </div>
 
           <div className="flex">
@@ -53,31 +51,31 @@ export default function home() {
               <Image
                 src={Temporal}
                 alt="Logo de la App"
-                className="row-start-1 row-end-3 rounded-tl-[20px] rounded-bl-[20px] object-cover"
+                className=""
                 width={20}
                 height={20}
               />
             </div>
 
-            <p className="text-md mx-5">Temporal</p>
+            <p className="text-md mx-2">Temporal</p>
           </div>
         </div>
       </section>
 
-      <section className="flex justify-center mt-5 w-48">
-        <div className="flex items-center ">
+      <section className="w-[324px] flex mt-5 mx-auto ">
+        <div className="flex items-center border-2 rounded-full  bg-[#ffffff] shadow-xl p-3 w-full">
           <Image
             src={Search}
             alt="Logo de la App"
-            className="row-start-1 row-end-3 rounded-tl-[20px] rounded-bl-[20px] object-cover"
-            width={20}
-            height={20}
+            className=""
+            width={15}
+            height={15}
           />
 
           <input
             type="text"
             placeholder="¿Dónde querés mudarte?"
-            className="flex w-24"
+            className="flex mx-2 w-[200px] text-sm"
           />
           <button className="bg-blue-500 text-slate-100 rounded-md">
             Buscar
@@ -94,20 +92,20 @@ export default function home() {
             width={330}
             height={172}
           />
-          <button className="bg-blue-500 text-slate-100 rounded-md">
+          <button className="bg-blue-500 text-slate-100 rounded-md ">
             Ver la lista
           </button>
         </div>
       </section>
 
       <section>
-        <div className="flex max-w-[318px] max-h-[136px] bg-[#ffffff]  mt-8 shadow-xl rounded-lg border p-4 ">
+        <div className="flex max-w-[328px] max-h-[136px] bg-[#ffffff]  mt-8 shadow-xl rounded-lg border p-4 mx-auto">
           <div className="flex ">
             <div>
-              <p className="font-semibold text-lg">
+              <p className="font-semibold text-[14px]">
                 Crea una cuenta con tus datos personales
               </p>
-              <p>
+              <p className="text-[14px]">
                 Regístrate y crea una cuenta con nosotros. Ingresa al mundo de
                 los inmuebles de la mejor manera posible.
               </p>
@@ -122,10 +120,10 @@ export default function home() {
           </div>
         </div>
 
-        <div className="flex max-w-[318px] max-h-[136px] bg-[#ffffff]  mt-8 shadow-xl rounded-lg border p-4 ">
+        <div className="flex max-w-[328px] max-h-[136px] bg-[#ffffff]  mt-8 shadow-xl rounded-lg border p-4 mx-auto">
           <div className="flex ">
             <div>
-              <p className="font-semibold text-lg">
+              <p className="font-semibold">
                 Busca y descubre millones de ofertas
               </p>
               <p>
@@ -143,10 +141,10 @@ export default function home() {
           </div>
         </div>
 
-        <div className="flex max-w-[318px] max-h-[136px] bg-[#ffffff] mt-8 shadow-xl rounded-lg border p-4 ">
+        <div className="flex max-w-[328px] max-h-[136px] bg-[#ffffff] mt-8 shadow-xl rounded-lg border p-4 mx-auto">
           <div className="flex ">
             <div>
-              <p className="font-semibold text-lg">
+              <p className="font-semibold ">
                 Conecta con la casa de tus sueños
               </p>
               <p>
@@ -164,6 +162,6 @@ export default function home() {
           </div>
         </div>
       </section>
-    </div>
+    </main>
   );
 }
