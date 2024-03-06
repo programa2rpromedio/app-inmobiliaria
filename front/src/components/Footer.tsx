f6ec0297b9045dbeb3ad58ca04f0ad7b5b1f1b7c
Sebastian Gallego <sebastian.gallego@gmail.com>
2024-03-06 10:37:15 -0300

    footer responsive

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -19,10 +19,10 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="bg-[#EDEEF2] mt-4 pt-5">
-      <div className="flex justify-between px-4">
+      <div className="flex justify-between px-4 md:ml-20 md:justify-start  md:gap-12">
         <div>
-          <p className="text-[10px] font-bold">Acerca de</p>
-          <ul className="mt-3 text-[10px] text-slate-9">
+          <p className="text-[10px] md:text-[18px] font-bold">Acerca de</p>
+          <ul className="mt-3 text-[10px] md:text-[18px] text-slate-9">
             <li className="mb-2">
               <Link href="">Alquilar</Link>
             </li>
@@ -35,8 +35,8 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <p className="text-[10px] font-bold">Contacto</p>
-          <ul className="mt-3 text-[9px] text-slate-9">
+          <p className="text-[10px] md:text-[18px]  font-bold">Contacto</p>
+          <ul className="mt-3 text-[9px] md:text-[18px]  text-slate-9">
             <li className="mb-2">
               <Link href="">Centro de Ayuda</Link>
             </li>
@@ -46,8 +46,10 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <p className="text-[10px] font-bold">Información Legal</p>
-          <ul className="mt-3 text-[10px] text-slate-9">
+          <p className="text-[10px] md:text-[18px]  font-bold">
+            Información Legal
+          </p>
+          <ul className="mt-3 text-[10px] md:text-[18px] text-slate-9">
             <li className="mb-2">
               <Link href="">Términos y condiciones</Link>
             </li>
@@ -60,80 +62,84 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="flex justify-end gap-4 px-4 mt-4 items-center">
+      <div className="flex justify-end gap-4 px-4 mt-4 items-center mr-20 md:mb-10">
         <Select>
-          <SelectTrigger className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] font-semibold outline-none">
+          <SelectTrigger className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] md:w-[234px] md:h-[56px] md:text-[20px] md:rounded-[15px]  font-semibold outline-none">
             <SelectValue placeholder="AR$" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup className="text-[9px] font-semibold">
               <SelectLabel></SelectLabel>
-              <SelectItem value="pesos$">AR$</SelectItem>
-              <SelectItem value="doalres">u$s</SelectItem>
-              <SelectItem value="reales">r$</SelectItem>
-              <SelectItem value="euros">euros</SelectItem>
+              <SelectItem
+                value="pesos$"
+                className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] md:w-[234px] md:h-[56px] md:text-[20px] md:rounded-[15px]  font-semibold outline-none"
+              >
+                AR$
+              </SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
         <Select>
-          <SelectTrigger className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] font-semibold outline-none">
+          <SelectTrigger className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] md:w-[234px] md:h-[56px] md:text-[20px] md:rounded-[15px] font-semibold outline-none">
             <SelectValue placeholder="Argentina" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup className="text-[9px] font-semibold">
               <SelectLabel></SelectLabel>
-              <SelectItem value="argentina">Argentina</SelectItem>
-              <SelectItem value="brasil">Brasil</SelectItem>
-              <SelectItem value="chile">Chile</SelectItem>
-              <SelectItem value="uruguay">Uruguay</SelectItem>
-              <SelectItem value="paraguay">Paraguay</SelectItem>
+              <SelectItem
+                value="argentina"
+                className="bg-[#ffffff]  w-[90px] h-[20px] text-[9px] md:w-[234px] md:h-[56px] md:text-[20px] md:rounded-[15px]  font-semibold outline-none"
+              >
+                Argentina
+              </SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
       </div>
       <hr className=" mt-4 border-[#C2C0C7]" />
-      <div className="flex p-4">
-        <div>
-          <div className="">
+      <div className="flex p-4 md:py-10 md:px-24">
+        <div className="flex items-center">
+          <div className=" ">
             <Image
               src={Logo}
               alt="Logo de la App"
-              className=""
+              className="md:w-[72px] md:h-[72px]"
               width={25}
               height={25}
             />
           </div>
+          <div className="text-[8px] mx-2 md:text-[16px] ">
+            <p>2024 Alquileres Ya!</p>
+            <p>Todos los derechos reservados</p>
+          </div>
         </div>
-        <div className="text-[8px] mx-2">
-          <p>2024 Alquileres Ya!</p>
-          <p>Todos los derechos reservados</p>
-        </div>
+
         <div className="flex ml-auto gap-4">
           <Image
             src={Facebook}
             alt="Icono Facebook"
-            className=""
+            className="md:w-[64px] md:h-[64px]"
             width={24}
             height={24}
           />
           <Image
             src={Instagram}
             alt="Icono Facebook"
-            className=""
+            className="md:w-[64px] md:h-[64px]"
             width={24}
             height={24}
           />
           <Image
             src={Twitter}
             alt="Icono Facebook"
-            className=""
+            className="md:w-[64px] md:h-[64px]"
             width={24}
             height={24}
           />
           <Image
             src={Youtube}
             alt="Icono Facebook"
-            className=""
+            className="md:w-[64px] md:h-[64px]"
             width={24}
             height={24}
           />
