37f99f74a2f828652e18470d4f5619ef08f666a0
programa2rpromedio <programadorpromedio@gmail.com>
2024-03-06 10:03:12 -0300

    header

--- front/src/app/propiedades/page.tsx
+++ front/src/app/propiedades/page.tsx
@@ -1,11 +1,15 @@
 import CardProperty from "@/components/CardProperty";
+import Header from "@/components/Header";
 
 export default function page() {
   return (
-    <main className="w-8/12 mx-auto border border-black p-4 grid sm:grid-cols-2 md:grid-cols-3 gap-9">
-      <CardProperty />
-      <CardProperty />
-      <CardProperty />
-    </main>
+    <>
+      <Header />
+      <main className="w-8/12 mx-auto border border-black p-4 grid sm:grid-cols-2 md:grid-cols-3 gap-9">
+        <CardProperty />
+        <CardProperty />
+        <CardProperty />
+      </main>
+    </>
   )
 }
