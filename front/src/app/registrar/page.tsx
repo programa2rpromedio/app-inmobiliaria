faca4c46fbd8332d73a3819c19756d5069da80be
programa2rpromedio <programadorpromedio@gmail.com>
2024-03-25 16:01:32 -0300

    responsive img register

--- front/src/app/registrar/page.tsx
+++ front/src/app/registrar/page.tsx
@@ -6,7 +6,8 @@ export default function page() {
     <main className='grid grid-cols-1  sm:grid-cols-2 min-h-screen'>
       <Register />
 
-      <div className='bg-register h-[250px] bg-no-repeat w-[250px] m-auto bg-contain  sm:bg-cover sm:bg-left-bottom sm:h-[60vh] lg:h-[80vh] sm:w-full lg:bg-contain'>
+      <div className='bg-register bg-center h-[350px] bg-no-repeat w-full m-auto bg-contain  sm:bg-cover sm:bg-left-bottom sm:h-[60vh] lg:h-[80vh] sm:w-full xl:bg-contain xl:h-[90vh]'>
+
 
       </div>
     </main>
