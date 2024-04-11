4ac7f7fe9070306916434f0bb46a925d71785017
programa2rpromedio <programadorpromedio@gmail.com>
2024-04-11 13:40:39 -0300

    url api deploy in vercel

--- front/src/lib/axios.ts
+++ front/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const baseURL = 'http://localhost:8080/api'
+// export const baseURL = 'http://localhost:8080/api'
+export const baseURL = 'https://api-alquileresya.vercel.app/api'
 
 // Establecer configuraciones por defecto al crear la instancia
 export const instanceAxios = axios.create({
